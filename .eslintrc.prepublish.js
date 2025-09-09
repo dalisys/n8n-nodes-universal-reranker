3e443a1498417b0c0d6adace3ecd0fd06353ec5e
Dali <daly.devs@gmail.com>
2025-09-10 00:51:37 +0200

    Update ESLint configuration to disable specific rules for node class descriptions

--- .eslintrc.prepublish.js
+++ .eslintrc.prepublish.js
@@ -12,6 +12,8 @@ module.exports = {
   extends: ['plugin:n8n-nodes-base/nodes', 'plugin:n8n-nodes-base/credentials'],
   rules: {
     'n8n-nodes-base/cred-class-field-documentation-url-miscased': 'off',
+    'n8n-nodes-base/node-class-description-inputs-wrong-regular-node': 'off',
+    'n8n-nodes-base/node-class-description-outputs-wrong': 'off',
   },
   ignorePatterns: ['dist/**'],
 };
